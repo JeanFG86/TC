5ce260c79a55ce5ee2534460132b08182cf6a18d
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-04-28 18:14:44 -0300

    definindo interaces de serviço

---
+++ src/TC.Busines/Interfaces/IFornecedorService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TC.Busines.Models;
+
+namespace TC.Busines.Interfaces;
+public interface IFornecedorService : IDisposable
+{
+    Task Adicionar(Fornecedor forncedor);
+    Task Atualizar(Fornecedor forncedor);
+    Task Remover(Guid id);
+}
---
+++ src/TC.Busines/Interfaces/IProdutoService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TC.Busines.Models;
+
+namespace TC.Busines.Interfaces;
+public interface IProdutoService : IDisposable
+{
+    Task Adicionar(Produto produto);
+    Task Atualizar(Produto produto);
+    Task Remover(Guid id);
+}
