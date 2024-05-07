de5825f13561d5ad9d0ee8cdb377464884ab0b84
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-05-07 19:24:07 -0300

    criacao do mapeamento do produto

---
+++ src/TC.Data/Mappings/ProdutoMapping.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TC.Busines.Models;
+
+namespace TC.Data.Mappings;
+public class ProdutoMapping : IEntityTypeConfiguration<Produto>
+{
+    public void Configure(EntityTypeBuilder<Produto> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Nome)
+            .IsRequired()
+            .HasColumnType("varchar(200)");
+        builder.Property(x => x.Descricao)
+            .IsRequired()
+            .HasColumnType("varchar(1000)");
+
+        builder.ToTable("Produtos");
+    }
+}
