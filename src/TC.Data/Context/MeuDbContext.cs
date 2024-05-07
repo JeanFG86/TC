41134e13fba1e98cb7794b48acecfd7f5790acef
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-05-07 18:55:54 -0300

    definindo o contexto do entity framework

---
+++ src/TC.Data/Context/MeuDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TC.Busines.Models;
+
+namespace TC.Data.Context;
+public class MeuDbContext : DbContext
+{
+    public MeuDbContext(DbContextOptions<MeuDbContext> options): base(options)
+    {
+        
+    }
+
+    public DbSet<Produto> Produtos { get; set; }
+    public DbSet<Endereco> Enderecos { get; set; }
+    public DbSet<Fornecedor> Fornecedores { get; set; }
+}
