a0ac1f1372730edf2381c4cf60728014fbcb2c6e
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-05-13 19:08:13 -0300

    configurando automapper

---
+++ src/TC.Api/Configs/AutoMapperConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using TC.Api.ViewModels;
+using TC.Busines.Models;
+
+namespace TC.Api.Configs;
+
+public class AutoMapperConfig : Profile
+{
+    public AutoMapperConfig()
+    {
+        CreateMap<Fornecedor, FornecedorViewModel>().ReverseMap();
+        CreateMap<Endereco, EnderecoViewModel>().ReverseMap();
+        CreateMap<ProdutoViewModel, Produto>();
+
+        CreateMap<Produto, ProdutoViewModel>()
+            .ForMember(x => x.NomeFornecedor, y => y.MapFrom(j => j.Fornecedor.Nome));
+    }
+}
